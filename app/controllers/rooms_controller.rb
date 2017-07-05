ba0601e73bf8a7afb72b323fd0f4f96a74bf741a
The Tien <tiendt2311@gmail.com>
2017-07-05 19:26:04 +0700

    Finised milestone 2

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,18 @@
+class RoomsController < ApplicationController
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      flash[:success] = "Room #{@room.name} created!"
+    else
+      flash[:error] = "Error #{@room.errors.full_messages.to_sentence}"
+    end
+    
+    redirect_to root_path
+  end
+
+  private
+    def room_params
+      params.require(:room).permit(:name)
+    end
+end
